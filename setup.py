d4e84850bb89e2941c242b63522917596d9e6ff3
Bobby Christopher <bobby@ixisdigital.com>
2022-01-22 00:51:21 -0500

    first pass at a base model for an orm

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="notion_orm",
+    version="0.0.1",
+    author="Bobby Christopher",
+    author_email="rec3college@gmail.com",
+    description="A base model for interacting with the Notion API'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/potofpie/notion-orm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	install_requires=[
+        'notion-database==20210513.4',
+        'python-dotenv==0.17.1'
+    ]
+)
